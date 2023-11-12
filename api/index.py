ac1d923965978e62d1ab1971342765d2227fe79f
archontmail <150447596+archontmail@users.noreply.github.com>
2023-11-12 19:09:03 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -11,5 +11,6 @@ def data():
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
-        json = request.form['text']
-        return render_template('data.html',form_data = form_data)
+        info = request.form['info']
+        prices = request.form['info']
+        return render_template('data.html',info = info, prices = prices)
