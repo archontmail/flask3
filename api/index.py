3c05772f73adc79114c75831d58e53f16444f907
archontmail <150447596+archontmail@users.noreply.github.com>
2023-11-12 15:41:12 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
-from flask import Flask
-
+from flask import Flask,render_template,request
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+ 
+ 
+app.run(host='localhost', port=5000)
